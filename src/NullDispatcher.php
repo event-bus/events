d44b24dbfd1b5e94cb88c4dcf02cab900a5ecc71
Thibaud Fabre <thibaud@evaneos.com>
2014-03-10 18:17:16 +0100

    Fix tabs (replace by spaces)

--- src/NullDispatcher.php
+++ src/NullDispatcher.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Evaneos\Events;
 
 class NullDispatcher implements EventDispatcher
@@ -7,20 +6,21 @@ class NullDispatcher implements EventDispatcher
 
     private $verbose = false;
 
-    public function __construct($verbose = false) {
+    public function __construct($verbose = false)
+    {
         $this->verbose = (bool) $verbose;
     }
 
-    public function addListener($category, EventSubscriber $subscriber) {
-		// Do nothing ! As my name implies....
-	}
+    public function addListener($category, EventSubscriber $subscriber)
+    {
+        // Do nothing ! As my name implies....
+    }
 
-	public function dispatch(Event $event) {
-		// Do... guess ? Nothing ! Ok, maybe just mention we got an event...
-		if ($this->verbose) {
-		    echo 'Got an event : ' . $event->getCategory() . ', yeah !', PHP_EOL;
-		}
-	}
-
-
+    public function dispatch(Event $event)
+    {
+        // Do... guess ? Nothing ! Ok, maybe just mention we got an event...
+        if ($this->verbose) {
+            echo 'Got an event : ' . $event->getCategory() . ', yeah !', PHP_EOL;
+        }
+    }
 }
