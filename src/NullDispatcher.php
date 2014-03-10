55f33f5a070017e1187e79d371220ba0314c8772
Thibaud Fabre <thibaud@evaneos.com>
2014-03-10 18:11:11 +0100

    Add null event dispatcehr

---
+++ src/NullDispatcher.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Evaneos\Events;
+
+class NullDispatcher implements EventDispatcher
+{
+
+    private $verbose = false;
+
+    public function __construct($verbose = false) {
+        $this->verbose = (bool) $verbose;
+    }
+
+    public function addListener($category, EventSubscriber $subscriber) {
+		// Do nothing ! As my name implies....
+	}
+
+	public function dispatch(Event $event) {
+		// Do... guess ? Nothing ! Ok, maybe just mention we got an event...
+		if ($this->verbose) {
+		    echo 'Got an event : ' . $event->getCategory() . ', yeah !', PHP_EOL;
+		}
+	}
+
+
+}
